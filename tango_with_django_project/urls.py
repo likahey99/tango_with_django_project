dd0868081831c03af2305318197fe6aaa6af62a7
Kai Hely <2661378h@student.gla.ac.uk>
2023-02-08 12:20:13 +0000

    reverted back to chapter 3 and fixed it to pass tests

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from django.conf import settings
-from django.conf.urls.static import static
 
 from rango import views
 
@@ -25,9 +23,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
 
-    # above maps any URLs starting
-    # with rango/ to be handled by
-    # the rango application
     path('admin/', admin.site.urls)
 ]
 
